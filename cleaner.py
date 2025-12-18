7baa3d23b0fdb8e71d25d88f1923904d03cfb107
IvoFaltus <ivosek.faltus@gmail.com>
2025-12-18 23:34:53 +0100

    fixes

--- cleaner.py
+++ cleaner.py
@@ -6,25 +6,17 @@ import re
 
 def strip_comments(code, lang):
     if lang == "python":
-        # remove # comments
         code = re.sub(r"#.*", "", code)
-        # remove triple-quoted docstrings
         code = re.sub(r'("""|\'\'\')(.*?)\1', "", code, flags=re.DOTALL)
     elif lang == "c" or lang == "cpp" or lang == "java":
-        # remove // comments
         code = re.sub(r"//.*", "", code)
-        # remove /* */ comments
         code = re.sub(r"/\*.*?\*/", "", code, flags=re.DOTALL)
     elif lang == "html":
-        # remove <!-- --> comments
         code = re.sub(r"<!--.*?-->", "", code, flags=re.DOTALL)
     elif lang == "js":
-        # remove // comments
         code = re.sub(r"//.*", "", code)
-        # remove /* */ comments
         code = re.sub(r"/\*.*?\*/", "", code, flags=re.DOTALL)
     elif lang == "css":
-        # remove /* */ comments
         code = re.sub(r"/\*.*?\*/", "", code, flags=re.DOTALL)
     return code
 
